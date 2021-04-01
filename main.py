d7f767701d56e50e2f0c1c4d5c7b2f762071a0a8
Easton Painter <eastonpainter@gmail.com>
2021-04-01 14:31:00 -0500

    clean output of all occurences

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import re
 import numpy as np
+import sys
 
 with open('restock-logs2021-03-21-to-2021-03-28.txt', 'r') as file:
     txt = file.read()
+
+# Indexed dict containing all usernames and occurences of said usernames
+indexed = {}
+lines_arr_nn = np.array([])
+
+np.set_printoptions(threshold=sys.maxsize)
 
 lines_arr = np.array(re.findall("\n.+ has just restocked one item in the shop!\n", txt))
 
@@ -11,17 +18,17 @@ lines_arr = np.array(re.findall("\n.+ has just restocked one item in the shop!\n
 #     lines_arr_str += lines_arr[i]
 
 # lines_arr_nn = re.sub("!\n", "!", lines_arr_str) 
-lines_arr_nn = []
+# Traverses through unfilted array, trimming values with regexps
 for i in range(len(lines_arr)):
-    lines_arr_nn.append(re.sub("\n", "", lines_arr[i])) 
-    lines_arr_nn.put(lines_arr_nn, lines_arr_nn[i], re.sub(" has just restocked one item in the shop!", "", lines_arr_nn[i]))
+    lines_arr_nn = np.append(lines_arr_nn, re.sub("\n", "", lines_arr[i])) 
+    np.put(lines_arr_nn, i, re.sub(" has just restocked one item in the shop!", "", lines_arr_nn[i]))
 
 
 #print(lines_arr_nn)
 
 #user_list_str = re.sub(" has just restocked one item in the shop!", "", lines_arr_nn)
 
-print(lines_arr_nn)
+print(np.array2string(lines_arr_nn, separator=', '))
 
 """
 print(lines_arr)
