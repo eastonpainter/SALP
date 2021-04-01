1d89aab4eddee9fb747b86b412cdc92bf8c9d614
Easton Painter <eastonpainter@gmail.com>
2021-04-01 13:53:31 -0500

    returned output to array, minus newline

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ with open('restock-logs2021-03-21-to-2021-03-28.txt', 'r') as file:
 
 lines_arr = re.findall("\n.+ has just restocked one item in the shop!\n", txt)
 
-lines_arr_str = ''
-for i in range(len(lines_arr)):
-    lines_arr_str += lines_arr[i]
+# lines_arr_str = ''
+# for i in range(len(lines_arr)):
+#     lines_arr_str += lines_arr[i]
 
-lines_arr_nn = re.sub("!\n", "!", lines_arr_str) 
+# lines_arr_nn = re.sub("!\n", "!", lines_arr_str) 
+lines_arr_nn = []
+for i in range(len(lines_arr)):
+    lines_arr_nn.append(re.sub("\n", "", lines_arr[i])) 
+    # lines_arr_nn.append(re.sub(" has just restocked one item in the shop!", "", lines_arr[i]))
+
 
 #print(lines_arr_nn)
 
-user_list_str = re.sub(" has just restocked one item in the shop!", "", lines_arr_nn)
+#user_list_str = re.sub(" has just restocked one item in the shop!", "", lines_arr_nn)
 
-print(user_list_str)
+print(lines_arr_nn)
 
 """
 print(lines_arr)
