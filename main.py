8c8a9f50e46b42681a8f654b69eb0a2875a61bae
Easton Painter <eastonpainter@gmail.com>
2021-04-03 20:08:51 -0500

    fixed encoding typo (again)

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ try:
 
     # March logs input
     if log == "mar" or log == "1" or log == "m1":
-        with open('restock-logs2021-03-21-to-2021-03-28.txt', 'r', encoding='ut8') as file:
+        with open('restock-logs2021-03-21-to-2021-03-28.txt', 'r', encoding='utf8') as file:
             txt = file.read()
 
     # February logs input
     elif log == "feb" or log == "2" or log == "f":
-        with open('restock-logs2021-02-14-to-2021-02-19.txt', 'r', encoding='ut8') as file:
+        with open('restock-logs2021-02-14-to-2021-02-19.txt', 'r', encoding='utf8') as file:
             txt = file.read()
 
     # Complete March logs input
     elif log == "mar2" or log == "3" or log == "m2":
-        with open('restock-logs2021-02-28-to-2021-04-01.txt', 'r', encoding='ut8') as file:
+        with open('restock-logs2021-02-28-to-2021-04-01.txt', 'r', encoding='utf8') as file:
                 txt = file.read()
 
     # Untruncates numpy array output
